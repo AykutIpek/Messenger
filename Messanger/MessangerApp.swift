03e1e5cd0e5a44a782c60b3b9a055e17413bce6a
AykutIpek <aykutipek_2001@hotmail.com>
2023-07-27 12:45:28 +0300

    First Commit

---
+++ Messanger/MessangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerApp.swift
+//  Messanger
+//
+//  Created by aykut ipek on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MessangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
