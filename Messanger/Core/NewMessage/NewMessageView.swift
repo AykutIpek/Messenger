5fbc2b0c7929e8b455714125c1c055a9cb43af81
AykutIpek <aykutipek_2001@hotmail.com>
2023-07-29 13:40:48 +0300

    New Message View UI Tasked

---
+++ Messanger/Core/NewMessage/NewMessageView.swift
@@ -0,0 +1,70 @@
+//
+//  NewMessageView.swift
+//  Messanger
+//
+//  Created by aykut ipek on 29.07.2023.
+//
+
+import SwiftUI
+
+struct NewMessageView: View {
+    //MARK: - Properties
+    @State private var searchText: String = ""
+    @Environment(\.dismiss) var dismiss
+    
+    //MARK: - Body
+    var body: some View {
+        NavigationStack {
+            ScrollView{
+                TextField("To: ", text: $searchText)
+                    .frame(height: 44)
+                    .padding(.leading)
+                    .background(Color(.systemGroupedBackground))
+                    .padding(.horizontal, 12)
+                Text("CONTACTS")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                ForEach(0 ... 10, id: \.self) { user in
+                    VStack {
+                        HStack {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            .foregroundColor(Color(.systemGray))
+                            Text("Chadwick Bozeman")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            Spacer()
+                        }
+                        .padding(.leading)
+                        
+                        Divider()
+                            .padding(.leading, 40)
+                    }
+                }
+            }
+            .navigationTitle("New Message")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel"){
+                        dismiss()
+                    }
+                    .foregroundColor(.black)
+                }
+        }
+        }
+    }
+}
+
+//MARK: - Previews
+struct NewMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            NewMessageView()
+        }
+    }
+}
